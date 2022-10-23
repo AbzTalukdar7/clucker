394f3073975c5fcecf121f6def331112e739db0c
Abed Talukdar <talukdar.a@hotmail.com>
2022-10-23 22:49:28 +0100

    add sign up form

--- microblogs/views.py
+++ microblogs/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .forms import signUpForm
 
 def home(request):
     return render(request, 'home.html')
+
+def sign_up(request):
+    form = signUpForm()
+    return render(request, 'sign_up.html',{'form':form})
